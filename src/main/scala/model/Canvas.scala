09370c09573a7dedede956f534e09214fdd5bee2
Wenxin Liu <wenxin.liu@springernature.com>
2021-04-14 01:28:35 +0100

    Refactor canvas

--- src/main/scala/model/Canvas.scala
+++ src/main/scala/model/Canvas.scala
@@ -1,5 +1,23 @@
 package model
 
 class Canvas {
-  def drawCanvas = List(10, 10)
+  def drawCanvas =
+    List(
+      List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+      List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+      List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+      List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+      List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+      List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+      List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+      List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+      List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+      List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    )
+
+  def updateCanvas(start: (Int, Int), end: (Int, Int), canvas: List[List[Int]]): List[List[Int]] = {
+    if (start == end)
+      canvas.updated(start._2-1, canvas(start._2-1).updated(start._1-1, 1))
+    else canvas
+  }
 }
--- src/test/scala/model/CanvasTest.scala
+++ src/test/scala/model/CanvasTest.scala
@@ -5,8 +5,36 @@ import org.scalatest.matchers.should.Matchers
 
 class CanvasTest extends AnyFlatSpec with Matchers{
   "A canvas" should "be 10 by 10" in {
-    val canvas = new Canvas
-
-    canvas.drawCanvas shouldBe List(10, 10)
+    canvas.drawCanvas shouldBe
+      List(
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+      )
   }
+
+  it should "have a submarine (1x1) at 2, 7" in {
+    canvas.updateCanvas(start = (2, 7), end = (2, 7), canvas = canvas.drawCanvas) shouldBe
+      List(
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 1, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+      )
+  }
+
+  lazy val canvas = new Canvas
 }
