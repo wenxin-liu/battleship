f231e4004f92da55fac091d8a32eea5297cf8f9a
Wenxin Liu <wenxin.liu@springernature.com>
2021-04-14 03:33:49 +0100

    Implement horizontal ships

--- src/main/scala/model/Canvas.scala
+++ src/main/scala/model/Canvas.scala
@@ -16,8 +16,24 @@ class Canvas {
     )
 
   def updateCanvas(start: (Int, Int), end: (Int, Int), canvas: List[List[Int]]): List[List[Int]] = {
+    val lineIsHorizontal = end._1 - start._1 != 0 && end._2 - start._2 == 0
+    val lineIsVertical = end._1 - start._1 == 0 && end._2 - start._2 != 0
+
     if (start == end)
       canvas.updated(start._2-1, canvas(start._2-1).updated(start._1-1, 1))
-    else canvas
+    else if (start != end) {
+      if (lineIsHorizontal) {
+        var newCanvas: List[List[Int]] = canvas
+        for (i <- start._1 - 1 to end._1 - 1) {
+          newCanvas = updateHorizontalShip(i, start._2-1, newCanvas)
+        }
+        newCanvas
+      } else if (lineIsVertical) {
+        ???
+      } else canvas
+    } else canvas
+}
+  def updateHorizontalShip(x: Int, y: Int, canvas: List[List[Int]]): List[List[Int]] = {
+    canvas.updated(y, canvas(y).updated(x, 1))
   }
 }
--- src/test/scala/model/CanvasTest.scala
+++ src/test/scala/model/CanvasTest.scala
@@ -20,7 +20,7 @@ class CanvasTest extends AnyFlatSpec with Matchers{
       )
   }
 
-  it should "have a submarine (1x1) at 2, 7" in {
+  it should "have a submarine (1 cell) at 2,7" in {
     canvas.updateCanvas(start = (2, 7), end = (2, 7), canvas = canvas.drawCanvas) shouldBe
       List(
         List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
@@ -31,6 +31,38 @@ class CanvasTest extends AnyFlatSpec with Matchers{
         List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
         List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
         List(0, 1, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+      )
+  }
+
+  it should "have a battleship (4 cells) at 2,3 to 5,3" in {
+    canvas.updateCanvas(start = (2, 3), end = (5, 3), canvas = canvas.drawCanvas) shouldBe
+      List(
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 1, 1, 1, 1, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+      )
+  }
+
+  it should "have a cruiser (3 cells) at 6,7 to 8,7" in {
+    canvas.updateCanvas(start = (6, 7), end = (8, 7), canvas = canvas.drawCanvas) shouldBe
+      List(
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        List(0, 0, 0, 0, 0, 1, 1, 1, 0, 0),
+        List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
         List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
         List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
       )
